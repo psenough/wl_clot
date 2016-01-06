32c75070e5c8423654ffe780cc4602910f2bfeb4
Filipe Cruz <psenough@gmail.com>
2016-01-06 12:22:52 +0000

    add old 1v1 ladder

--- hub/list_config.php
+++ hub/list_config.php
@@ -33,17 +33,17 @@ $clots = [
 	/*[ 	
 		"url" => "http://real-time-ladder.appspot.com/lot/5649391675244544",
 		"type" => "realtime",
-		"url_type" => "fizzer_example"
-	],
+		"url_type" => "motd_template"
+	],*/
 	[ 	
 		"url" => "http://wl1v1-clot.appspot.com/lot/5629499534213120",
 		"type" => "multiday",
-		"url_type" => "fizzer_example"
+		"url_type" => "motd_template"
 	],
-	[ 
+	/*[ 
 		"url" => "http://multi-day-ladder.appspot.com/lot/5629499534213120",
 		"type" => "multiday",
-		"url_type" => "fizzer_example"
+		"url_type" => "motd_template"
 	],*/
 	[
 		"url" => "http://warladder.net",
@@ -63,7 +63,7 @@ foreach ($clots as $clot) {
 	$html = file_get_html($clot["url"]);
 	if ($html) {
 		switch($clot["url_type"]) {
-			case "fizzer_example":
+			case "motd_template":
 				//TODO: extract template info
 				$output[$i]['name'] = $html->find("h1", 0)->plaintext;
 				$output[$i]['players'] = count($html->find('.table',1)->find('tr'))-1;
