974cda7fa8ce9bb5439b67a10493472cb33c234b
Filipe Cruz <psenough@gmail.com>
2016-01-13 17:29:27 +0000

    old ladder is down again

--- hub/list_config.php
+++ hub/list_config.php
@@ -34,13 +34,13 @@ $clots = [
 		"url" => "http://real-time-ladder.appspot.com/lot/5649391675244544",
 		"type" => "realtime",
 		"url_type" => "motd_template"
-	],*/
+	],
 	[ 	
 		"url" => "http://wl1v1-clot.appspot.com/lot/5629499534213120",
 		"type" => "multiday",
 		"url_type" => "motd_template"
 	],
-	/*[ 
+	[ 
 		"url" => "http://multi-day-ladder.appspot.com/lot/5629499534213120",
 		"type" => "multiday",
 		"url_type" => "motd_template"
