6621fb1d0e51492678ca38ab411b9cfee12f081b
Filipe Cruz <psenough@gmail.com>
2016-07-12 23:44:57 +0100

    no message

--- hub/list_config.php
+++ hub/list_config.php
@@ -92,7 +92,7 @@ $leagues = [
 	[ 	
 		"url" => "https://www.warlight.net/Forum/166818-clan-league-8-links",
 		"name" => "Clan League 8",
-		"players" => "~150"
+		"players" => "~260"
 	],
 	[ 	
 		"url" => "https://www.warlight.net/Forum/164080-manager-league-season-2",
