cb6fad5d364045a8a78b43ad2ea978551b279676
Filipe Cruz <psenough@gmail.com>
2016-10-29 00:43:48 +0100

    http://md-ladder.cloudapp.net/

--- hub/list_config.php
+++ hub/list_config.php
@@ -30,11 +30,11 @@ if ($string) {
 
 
 $clots = [
-	[ 	
+	/*[ 	
 		"url" => "http://real-time-ladder.appspot.com/lot/5649391675244544",
 		"type" => "realtime",
 		"url_type" => "motd_template"
-	]/*,
+	],
 	[ 	
 		"url" => "http://wl1v1-clot.appspot.com/lot/5629499534213120",
 		"type" => "multiday",
@@ -50,6 +50,11 @@ $clots = [
 		"type" => "multiday",
 		"url_type" => "dutch"
 	]*/
+	[
+		"url" => "http://md-ladder.cloudapp.net/",
+		"type" => "multiday",
+		"url_type" => "motd_template"
+	]
 ];
 		
 $output = [];
